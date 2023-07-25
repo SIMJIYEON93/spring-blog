a9a3f0c8f88874bc693ed0cefec5d2d9864cc0d4
SIMJIYEON93 <930515s@gmail.com>
2023-07-25 14:12:25 +0900

    init

---
+++ src/main/java/com/sparta/springblog/dto/PostListResponseDto.java
@@ -0,0 +1,15 @@
+package com.sparta.springblog.dto;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class PostListResponseDto {
+    private List<PostResponseDto> postsList;
+
+    public PostListResponseDto(List<PostResponseDto> postList) {
+        this.postsList = postList;
+    }
+}
+
